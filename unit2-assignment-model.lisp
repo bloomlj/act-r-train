68db12eb539c53a7bfd7233f7e8a6524d16ded12
bloomlj <kollocc@live.com>
2019-12-19 09:13:29 +0800

    unit2的3个字母找不同的一个算是可以了
    不过有一些疑问还没有弄清楚，完全靠原始的一个个试搞出来的，没有具体的开发参考真实老火啊？也许有，我没找到？

--- unit2-assignment-model.lisp
+++ unit2-assignment-model.lisp
@@ -8,7 +8,7 @@
 (chunk-type read-letters state)
 (chunk-type array letter1 letter2 letter3)
 
-;定义缓冲区
+;定义陈述性知识
 (add-dm
 ;先定义一些slot，这些是state的具体情况的名词
  (start isa chunk)
@@ -17,12 +17,17 @@
  (attend isa chunk)
  (attend2 isa chunk)
  (attend3 isa chunk)
- (respond isa chunk)
+ (respond1 isa chunk)
+ (respond2 isa chunk)
+ (respond3 isa chunk)
  (done isa chunk)
+
 ;goal的陈述句式
  (goal isa read-letters state start))
 
 ;产生式
+;这里的过程我们可以看到，actr模型/也就是人识别图像的方法是三步走：
+;1、准备visual-location 2、move-attention（这里其实就识别出来了） 3、读取识别结果 encode
 ;如果任务开始，清理attended缓冲
  (P find-unattended-letter
     =goal>
@@ -102,10 +107,12 @@
        value       =letter2
     ?imaginal>
        state       free
+    =imaginal>
+       letter2     nil ;下面给imaginal存储letter2的时候，这里必须要进行test
  ==>
     =goal>
        state       start3
-    +imaginal>
+    =imaginal>
        isa         array
        letter2     =letter2
  )
@@ -145,22 +152,30 @@
         value       =letter3
      ?imaginal>
         state       free
+     =imaginal>;下面给imaginal存储letter3的时候，这里必须要进行test
+        letter3     nil
   ==>
      =goal>
         state       respond
-     +imaginal>
+     =imaginal>;这里必须用=，表示修改，可以新增一个letter3 slot和value、不能用+，否则会清理掉以前的数据，存储新的。
         isa         array
-        letter3      =letter3
+        letter3     =letter3
   )
 
+
  ;反馈，行动模块输出按键指令，参数是识别到的字母
- (P respond
+ ;如果letter2 = letter1 那么输出3
+
+ (P respond3
     =goal>
        ISA         read-letters
        state       respond
     =imaginal>
        isa         array
-       letter3      =letter3
+       letter1     =letter1
+       letter2     =letter2;再取出letter2的值，一定要先取出来，不然失败
+       letter2     =letter1;判断letter1的值是否和letter2是否是一样的，下面的respond产生式原理相同
+       letter3     =letter3
     ?manual>
        state       free
  ==>
@@ -171,6 +186,50 @@
        key         =letter3
  )
 
+ ;反馈，行动模块输出按键指令，参数是识别到的字母
+ ;如果letter3 = letter1 那么输出2
+
+ (P respond2
+    =goal>
+       ISA         read-letters
+       state       respond
+    =imaginal>
+       isa         array
+       letter1      =letter1
+       letter3      =letter3
+       letter3      =letter1
+       letter2      =letter2
+    ?manual>
+       state       free
+ ==>
+    =goal>
+       state       done
+    +manual>
+       cmd         press-key
+       key         =letter2
+ )
+ ;反馈，行动模块输出按键指令，参数是识别到的字母
+ ;如果letter3 = letter2 那么输出1
+
+ (P respond1
+    =goal>
+       ISA         read-letters
+       state       respond
+    =imaginal>
+       isa         array
+       letter3      =letter3 ;下面=letter3的时候不是取了，为啥要先取一下，不然不对呢？
+       letter2      =letter2
+       letter2      =letter3
+       letter1      =letter1
+    ?manual>
+       state       free
+ ==>
+    =goal>
+       state       done
+    +manual>
+       cmd         press-key
+       key         =letter1
+ )
 
 ;聚焦目标，干活
 (goal-focus goal)
